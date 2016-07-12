1312dfdf7421018e90d39588f04bebe9aefd5738
Emmanuel Villegas <evc.naq@hotmail.com>
2016-07-11 22:18:41 -0500

    agregamos browserify y babelify para poder hacer el bundle del app y que babel traduzca para gulp poder entender y realizar las tareas sin problemas

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
 
 gulp.task('styles', function() {
   gulp
   .src('index.scss')
   .pipe(sass())
+  .pipe(rename('app.css'))
   .pipe(gulp.dest('public'));
 });
 
@@ -12,6 +18,15 @@ gulp.task('assets', function () {
   gulp
   .src('assets/*')
   .pipe(gulp.dest('public'));
+});
+
+gulp.task('scripts', function () {
+  browserify('./src/index.js')
+    .transform(babel)//permite usar todo lo mas nuevo de es2015
+    .bundle()
+    .pipe(source('index.js'))//trasnformar lo que devuelve el bundle a algo que nentienda gulp
+    .pipe(rename('app.js'))
+    .pipe(gulp.dest('public'));
 })
 
-gulp.task('default', ['styles', 'assets'])
+gulp.task('default', ['styles', 'assets', 'scripts'])
