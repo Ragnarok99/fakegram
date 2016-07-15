cf3d5f342d493f8ae91efc85bb8a310e081cb5b5
Emmanuel Villegas <evc.naq@hotmail.com>
2016-07-14 22:22:32 -0500

    se modularizó el proyecto

--- public/app.js
+++ public/app.js
@@ -2367,17 +2367,34 @@ module.exports = [
 
 },{}],16:[function(require,module,exports){
 var page = require('page');
-var yo = require('yo-yo');
-var empty = require('empty-element');
-
-var main = document.getElementById('main-container');
 
 page('/', function (ctx, next) {
-  main.innerHTML = '<a href="/signup >signup</a>';
+    var main = document.getElementById('main-container');
+    main.innerHTML = '<a href="/signup >signup</a>';
 });
 
+},{"page":4}],17:[function(require,module,exports){
+var page = require('page');
+
+require('./homepage');
+require('./signup');
+
+page();
+
+},{"./homepage":16,"./signup":18,"page":4}],18:[function(require,module,exports){
+var page = require('page');
+var empty = require('empty-element');
+var template = require('./template');
+
 page('/signup', function (ctx, next) {
-  var el = yo`<div class="container">
+      var main = document.getElementById('main-container');
+      empty(main).appendChild(template);
+});
+
+},{"./template":19,"empty-element":3,"page":4}],19:[function(require,module,exports){
+var yo = require('yo-yo');
+
+module.exports = yo`<div class="container">
         <div class="row">
           <div class="col s10 push-s1">
             <div class="row">
@@ -2419,9 +2436,4 @@ page('/signup', function (ctx, next) {
         </div>
       </div>`;
 
-  empty(main).appendChild(el);
-});
-
-page();
-
-},{"empty-element":3,"page":4,"yo-yo":7}]},{},[16]);
+},{"yo-yo":7}]},{},[17]);
