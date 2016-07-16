5ce0a9ba620ecab8c11d2d1c94e6823323717860
Emmanuel Villegas <evc.naq@hotmail.com>
2016-07-16 12:21:09 -0500

    utilizamos formatJS para  internacionalizar las fechas ya que es más nativo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 var page = require('page');
-var moment = require('moment');
 
-moment.locale('es');
-
-require('moment/locale/es');
 require('./homepage');
 require('./signup');
 require('./signin');
