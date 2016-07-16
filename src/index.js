047df38643ac0ab612fc8bd63f9ed20431202e37
Emmanuel Villegas <evc.naq@hotmail.com>
2016-07-16 01:20:23 -0500

    fecha de la publicación implementada como moment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 var page = require('page');
+var moment = require('moment');
 
+moment.locale('es');
+
+require('moment/locale/es');
 require('./homepage');
 require('./signup');
 require('./signin');
