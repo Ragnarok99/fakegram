d604a85100f5d06a5af09a7ba5c6564b453e00c0
Emmanuel Villegas <evc.naq@hotmail.com>
2016-07-17 22:48:35 -0500

    hicimos un servicio(/api/pictures) para obtener la imagenes mediante el por medio de la lib superAgent

--- server.js
+++ server.js
@@ -17,6 +17,37 @@ app.get('/signin', function(req, res) {
   res.render('index', { title: 'Fakegram - signin'  });
 })
 
+app.get('/api/pictures', function (req, res, next) {
+  
+var pictures = [
+      {
+            user: {
+                  username: 'emma99',
+                  avatar: 'https://scontent-mia1-1.xx.fbcdn.net/v/t1.0-9/13511055_10207912026893391_6311686932057139332_n.jpg?oh=b50dd20c1980f67e126d15703cd40991&oe=581FE66E'
+            },
+            url: 'office.jpg',
+            likes: 0,
+            liked: true,
+            createdAt: new Date().getTime()
+      },
+      {
+            user: {
+                  username: 'perrim',
+                  avatar: 'https://scontent-mia1-1.xx.fbcdn.net/v/t1.0-9/13511055_10207912026893391_6311686932057139332_n.jpg?oh=b50dd20c1980f67e126d15703cd40991&oe=581FE66E'
+            },
+            url: 'office.jpg',
+            likes: 2,
+            liked: true,
+            createdAt: new Date().setDate(new Date().getDate() - 10)
+            
+      }
+];
+setTimeout(function(params) {
+  res.send(pictures);
+}, 2000);//simular el cargado de la BD
+
+})
+
 app.listen(3000, function(err) {
   if (err) return console.log('Hubo un error'), process.exit(1);
 
